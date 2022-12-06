2fae6503339a785797f1051f46efba4d8cfe1d31
jhosuappp <joshua@150porciento.com>
2022-12-06 14:42:14 -0500

    Finish firts section

--- 1.primero-pasos-counter/src/components/Events.jsx
+++ 1.primero-pasos-counter/src/components/Events.jsx
@@ -3,16 +3,21 @@ import PropTypes from "prop-types";
 
 export const EventClick = ({ initialState })=>{
 
-    let [ counter, setCounter ] = useState(initialState);
-    let counterMutado = 0;
-    const ClickEvent = ()=>{
-        setCounter( counter + 1 );
-    }
+    //Se renderiza el componente cuando cambia el estado
+    console.log('render');
+
+    const [ counter, setCounter ] = useState( initialState );
+    
+    const ClickEvent = ()=> setCounter( counter + 1 );
+    const ClickEventResta = ()=> setCounter( counter - 1 );
+    const ClickEventReset = ()=> setCounter( initialState );
 
     return (
         <>
-            <h1>Da click aquí</h1>
-            <button onClick={ ClickEvent }>Click + { counter }</button>
+            <h1>{ counter }</h1>
+            <button onClick={ ClickEvent }>+1</button>
+            <button onClick={ ClickEventResta }>-1</button>
+            <button onClick={ ClickEventReset }>reset</button>
         </>
     )
 }
