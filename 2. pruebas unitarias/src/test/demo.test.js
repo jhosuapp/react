963968d7b2397c8e968939c7b51073ba682c7270
jhosuappp <joshua@150porciento.com>
2022-12-06 17:04:19 -0500

    introduction to testing in react

---
+++ 2. pruebas unitarias/src/test/demo.test.js
@@ -0,0 +1,18 @@
+describe('pruebas en <micomponente />', ()=>{
+
+    test('Prueba textos iguales', ()=>{
+    
+        //DECLARACIÓN
+        const message1 = 'hola mundo';
+    
+        //ESTÍMULO
+        const message2 = message1.trim();
+    
+        //OBSERVAR EL COMPORTAMINETO ESPERADO
+        // expect( message1 ).toBe( message2 );
+        expect(message1).toBe( message2 );
+        
+    });
+    
+});
+
